23aefae1f531f1b3dbae577cb97f387a7e09790b
Ivelin Ivanov <ivelin.ivanov@Student01s-Mac-mini.local>
2013-07-22 12:37:56 +0300

    Initial Commit

---
+++ iTunesAppInfo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iTunesAppInfo
+//
+//  Created by Ivelin Ivanov on 7/22/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iTunesAppInfo/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  iTunesAppInfo
+//
+//  Created by Ivelin Ivanov on 7/22/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
